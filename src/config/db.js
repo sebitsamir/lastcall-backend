6c7eed368521f2e6c21912e86079786abcfbe6d8
erlking <sebitsamir@gmail.com>
2026-08-01 01:03:59 +0200

    Fixed the errors and now the backend is live

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,6 @@ const logger = require("../utils/logger");
 
 const connectDB = async () => {
 
-  console.log("=== RENDER ENV CHECK ===");
-  console.log("Does MONGO_URI exist?", process.env.MONGO_URI ? "YES" : "NO");
-  console.log("========================");
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       maxPoolSize: 10, //Connection pooling
